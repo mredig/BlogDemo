348b7d2b836d13f88d9f562b1ec53513661b3fc7
Michael Redig <mredig@gmail.com>
2019-05-07 00:02:23 -0500

    finished demo

--- BlogDemo/ViewController.swift
+++ BlogDemo/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var searchBar: UISearchBar!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		for subview in searchBar.allSubviews() {
+			if let selector = subview as? UISegmentedControl {
+				selector.tintColor = .yellow
+				subview.backgroundColor = .clear
+			}
+		}
 	}
 
 
