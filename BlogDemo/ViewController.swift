5d007f0ba4cc4991b5fce56c4ceecac358ba3cd2
Michael Redig <mredig@gmail.com>
2019-05-07 00:52:39 -0500

    jumbled the alphabet - sorry a, you're no longer first

--- BlogDemo/ViewController.swift
+++ BlogDemo/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		let screwyAlphabet = "qiwcghxkberdsljpvumyaoznft"
+
+		// closure 1
+		var sortedAlphabet = screwyAlphabet.sorted { (a, b) -> Bool in
+			return a < b
+		}
+
+		// closure 2
+		sortedAlphabet = screwyAlphabet.sorted(by: { (a, b) -> Bool in
+			a < b
+		})
+
+		// closure 3
+		sortedAlphabet = screwyAlphabet.sorted { $0 < $1 }
 	}
 
 
