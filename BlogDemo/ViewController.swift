feebeab0430f0e2d46b2ce1cc175565d4d83a492
Michael Redig <mredig@gmail.com>
2019-05-07 00:21:21 -0500

    successfully kinda crashes

--- BlogDemo/ViewController.swift
+++ BlogDemo/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var dataLabel: UILabel!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+
+		DispatchQueue.global().async {
+			self.dataLabel.text = "NEW IMPORTANT INFORMATION!!!"
+		}
 	}
 
 
